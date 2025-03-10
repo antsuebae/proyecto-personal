d312a8cfe2738aa07f80e23f6c9aee18203c2310
antsuebae <you@example.com>
2025-03-10 12:54:23 +0100

    Funcionalidad graficas

---
+++ windows/install.bat
@@ -0,0 +1,1 @@
+npm install axios@1.8.2 cheerio@1.0.0 cors@2.8.5 date-fns@4.1.0 dotenv@16.4.7 express@4.21.2 form-data@4.0.2 fs@0.0.1-security helmet@8.0.0 node-fetch@3.3.2 path@0.12.7 request@2.88.2
